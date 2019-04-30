e7f3534c4341fb606b9f819dd32ab3258a4a1c16
Luis Eduardo Rosales Prieto <nye12r@gmail.com>
2019-04-29 21:48:39 -0500

    Commit inicial

---
+++ holaMundo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  holaMundo
+//
+//  Created by Luis Eduardo Rosales Prieto on 3/25/19.
+//  Copyright © 2019 Luis Eduardo Rosales Prieto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
